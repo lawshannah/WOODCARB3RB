a36d8aa3e8d80f6fd0b815f569020fe17d9d6700
Alan Arnholt <arnholtat@appstate.edu>
2016-11-08 14:03:19 -0500

    tweakers

--- docs/pkgdown.js
+++ docs/pkgdown.js
@@ -1,20 +1,8 @@
 $(function() {
-<<<<<<< HEAD
   $("#sidebar").stick_in_parent({offset_top: 40});
   $('body').scrollspy({
     target: '#sidebar',
     offset: 60
-=======
-
-  $('#tocnav').affix({
-    offset: {
-      top: $('#tocnav').offset().top - 80
-    }
-  });
-  $('body').scrollspy({
-    target: '#tocnav',
-    offset: 80
->>>>>>> master
   });
 
 });
